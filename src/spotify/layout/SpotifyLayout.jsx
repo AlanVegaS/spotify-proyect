b6b660adaf4f609cabfef050c90939f29abc0fda
Alan Vega <alan.vega@outlook.es>
2024-11-30 21:06:58 -0600

    update new requests

--- src/spotify/layout/SpotifyLayout.jsx
+++ src/spotify/layout/SpotifyLayout.jsx
@@ -7,7 +7,7 @@ import { useWindowWidth } from '../hooks';
 
 export const SpotifyLayout = () => {
 
-  const { data } = useGetLibraryMostHeaderContentQuery();
+  const { data, isFetching } = useGetLibraryMostHeaderContentQuery();
   const dispatch = useDispatch();
   const windowWidth = useWindowWidth();
   const { sideBar, phoneMode } = useSelector(state => state.spotify);
@@ -19,27 +19,34 @@ export const SpotifyLayout = () => {
   }, [windowWidth, isModifiedByUser]);
 
   useEffect(() => {
-    const playlists = data?.playlists.items
-    if (playlists) {
+    let playlists = data?.playlists.items
+    if (!isFetching && playlists) {
+      playlists = playlists.filter(playlist => playlist !== null);
       dispatch(setMostHeaders(playlists.slice(0, 8)));
       dispatch(setLibrary(playlists.slice(8, playlists.length - 1)));
     }
-  }, [data]);
+  }, [data, isFetching]);
 
   return (
     <div className={`w-full h-screen gap-sm box-border p-sm bg-l-base-primary dark:bg-base-primary
-    grid grid-areas-standar-template grid-rows-standar-template xs:grid-areas-phone-template xs:grid-cols-xs-template
-    ${!isModifiedByUser
+      grid grid-areas-standar-template grid-rows-standar-template xs:grid-areas-phone-template xs:grid-cols-xs-template
+      ${!isModifiedByUser
         ? 'sm:grid-cols-sm-template md:grid-cols-md-template lg:grid-cols-lg-template xl:grid-cols-xl-template'
         : isModifiedByUser && isCompressed
           ? 'grid-cols-sm-template'
           : 'grid-cols-md-template lg:grid-cols-lg-template xl:grid-cols-xl-template'
-      }`}>
+      }`
+    }>
       {
-        !phoneMode && <SideBar></SideBar>
+        !isFetching &&
+        (<>
+          {
+            !phoneMode && <SideBar></SideBar>
+          }
+          <MainContent></MainContent>
+          <FooterPlayer></FooterPlayer>
+        </>)
       }
-      <MainContent></MainContent>
-      <FooterPlayer></FooterPlayer>
-    </div>
+    </div >
   );
 };
