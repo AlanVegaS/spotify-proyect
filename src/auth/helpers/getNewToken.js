05274d496e81f335fdcbbc6b9ce914cf01fc2800
Alan Vega <alan.vega@outlook.es>
2024-11-30 21:13:58 -0600

    change token request

--- src/auth/helpers/getNewToken.js
+++ src/auth/helpers/getNewToken.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getNewToken = async () => {
-    const response = await axios.get('http://localhost:8888/.netlify/functions/index/auth/spotify/token');
+    const response = await axios.post('https://auth-spotify.netlify.app/.netlify/functions/index/auth/spotify/token');
     const newToken = await response.data.access_token;
 
     return newToken;
